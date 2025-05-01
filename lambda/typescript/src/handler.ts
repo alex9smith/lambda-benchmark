b8abb48e17aad75f4eb9894962a80430141e0a5c
Alex Whitehead-Smith <alex.me.smith@gmail.com>
2025-05-01 08:35:38 +0100

    (ts): Sketch out function behaviour

--- lambda/typescript/src/handler.ts
+++ lambda/typescript/src/handler.ts
@@ -1,3 +1,38 @@
-export function add(a: number, b: number) {
-  return a + b;
+import { SQSEvent, Context } from "aws-lambda";
+import { Logger } from "@aws-lambda-powertools/logger";
+
+export interface Event {
+  eventId: string;
+  emitterCode: number;
+  action: string;
+  user: {
+    id: string;
+    sessionId: string;
+    deviceId?: string;
+  };
+}
+
+export function validateEvent(event: Event): void {}
+
+export async function writeEventToDynamoDb(event: Event): Promise<void> {}
+
+const logger = new Logger();
+
+export async function handler(
+  event: SQSEvent,
+  context: Context
+): Promise<void> {
+  logger.addContext(context);
+  await Promise.all(
+    event.Records.map(async (record) => {
+      try {
+        const event: Event = JSON.parse(record.body);
+        validateEvent(event);
+        await writeEventToDynamoDb(event);
+      } catch (error) {
+        logger.error((error as Error).message);
+        throw error;
+      }
+    })
+  );
 }
--- lambda/typescript/src/tests/handler.test.ts
+++ lambda/typescript/src/tests/handler.test.ts
@@ -1,10 +1,26 @@
-import { add } from "../handler";
+import { validateEvent, Event } from "../handler";
 
-describe("handler", () => {
-  describe("add", () => {
-    test("adds two numbers together", () => {
-      const result = add(1, 2);
-      expect(result).toEqual(3);
-    });
+const validEvent: Event = {
+  eventId: "event-id",
+  emitterCode: 50,
+  action: "sign_in",
+  user: {
+    id: "user-id",
+    sessionId: "session-id",
+    deviceId: "device-id",
+  },
+};
+
+describe("validateEvent", () => {
+  test("it doesn't throw an error for a valid event", () => {
+    expect(() => {
+      validateEvent(validEvent);
+    }).not.toThrow();
   });
+
+  test.todo("it throws an error when the event is missing a property");
+
+  test.todo(
+    "it throws an error when the event matches the interface but not the schema"
+  );
 });
