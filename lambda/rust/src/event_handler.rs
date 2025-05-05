a87a4e405a3b2ccf47f4a77c22fff2d808c08a19
Alex Whitehead-Smith <alex.me.smith@gmail.com>
2025-05-05 21:21:06 +0100

    (rust): Parse the event in lambda handler

--- lambda/rust/src/event_handler.rs
+++ lambda/rust/src/event_handler.rs
@@ -1,15 +1,29 @@
-use lambda_runtime::{tracing, Error, LambdaEvent};
 use aws_lambda_events::event::sqs::SqsEvent;
+use lambda_runtime::{Error, LambdaEvent};
+use serde::{Deserialize, Serialize};
+use serde_json;
 
-/// This is the main body for the function.
-/// Write your code inside it.
-/// There are some code example in the following URLs:
-/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-/// - https://github.com/aws-samples/serverless-rust-demo/
-pub(crate)async fn function_handler(event: LambdaEvent<SqsEvent>) -> Result<(), Error> {
+#[derive(Deserialize, Serialize, Default)]
+pub struct User {
+    id: String,
+    session_id: String,
+    devide_id: Option<String>,
+}
+
+#[derive(Deserialize, Serialize, Default)]
+pub struct Event {
+    event_id: String,
+    user: User,
+    emitter_code: u32,
+    action: String,
+}
+
+pub(crate) async fn function_handler(lambda_event: LambdaEvent<SqsEvent>) -> Result<(), Error> {
     // Extract some useful information from the request
-    let payload = event.payload;
-    tracing::info!("Payload: {:?}", payload);
+    let payload = lambda_event.payload;
+    for message in payload.records {
+        let event: Event = serde_json::from_str(&message.body.unwrap()).unwrap();
+    }
 
     Ok(())
 }
@@ -17,11 +31,37 @@ pub(crate)async fn function_handler(event: LambdaEvent<SqsEvent>) -> Result<(), 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use aws_lambda_events::sqs::SqsMessage;
     use lambda_runtime::{Context, LambdaEvent};
+    use std::collections::HashMap;
+
+    fn build_message(body: &str) -> SqsMessage {
+        SqsMessage {
+            message_id: None,
+            receipt_handle: None,
+            aws_region: None,
+            body: Some(body.to_string()),
+            md5_of_body: None,
+            md5_of_message_attributes: None,
+            event_source: None,
+            event_source_arn: None,
+            attributes: HashMap::new(),
+            message_attributes: HashMap::new(),
+        }
+    }
+
+    fn build_event(body: &str) -> SqsEvent {
+        SqsEvent {
+            records: vec![build_message(body)],
+        }
+    }
 
     #[tokio::test]
     async fn test_event_handler() {
-        let event = LambdaEvent::new(SqsEvent::default(), Context::default());
+        let event = LambdaEvent::new(
+            build_event(&serde_json::to_string(&Event::default()).unwrap()),
+            Context::default(),
+        );
         let response = function_handler(event).await.unwrap();
         assert_eq!((), response);
     }
