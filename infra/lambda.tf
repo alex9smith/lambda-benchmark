a9130cc5961360d13577698c6a3c648bb3fce50f
Alex Whitehead-Smith <alex.me.smith@gmail.com>
2025-05-07 15:04:28 +0100

    (rust): Deploy the Rust lambda
    The lambda doesn't yet write to DynamoDB, but it functions enough to be
    confident it's picking up events from SQS and validating the structure

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -7,3 +7,13 @@ module "typescript_lambda" {
   handler             = "index.handler"
   dynamodb_table_name = var.dynamodb_table_name
 }
+
+module "rust_lambda" {
+  source = "./modules/lambda"
+
+  source_file         = "../lambda/rust/target/lambda/rust/bootstrap.zip"
+  language_name       = "rust"
+  runtime             = "provided.al2023"
+  handler             = "bootstrap"
+  dynamodb_table_name = var.dynamodb_table_name
+}
