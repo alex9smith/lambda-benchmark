6cb84450996edf9902d506a2a7990b4a8878de82
Alex Whitehead-Smith <alex.me.smith@gmail.com>
2025-05-08 14:33:46 +0100

    (ruby): Validate the event against the schema

--- lambda/ruby/lib/lambda_function.rb
+++ lambda/ruby/lib/lambda_function.rb
@@ -1,7 +1,63 @@
+require "json"
+require "json-schema"
+
 module Lambda
-    class Handler
-        def self.add(first, second)
-            first + second
-        end
+  class Handler
+    def self.process(lambda_event:, context:)
+      lambda_event['Records'].each do |message|
+        event = JSON.parse(message)
+        self.validate_event(event)
+        self.write_to_dynamo_db(event)
+      end
     end
+
+    def self.validate_event(event)
+      JSON::Validator.validate!(Lambda.schema, event)
+    end
+
+    def self.write_to_dynamo_db(event)
+    end
+  end
+
+  def schema
+    {
+      "$schema": "http://json-schema.org/draft-04/schema#",
+      "type": "object",
+      "properties": {
+        "eventId": {
+          "type": "string",
+          "maxLength": 128
+        },
+        "emitterCode": {
+          "type": "integer",
+          "minimum": 0,
+          "maximum": 100
+        },
+        "action": {
+          "type": "string",
+          "enum": ["sign_in", "sign_out", "create_account", "delete_account"]
+        },
+        "user": {
+          "type": "object",
+          "properties": {
+            "id": {
+              "type": "string",
+              "maxLength": 128
+            },
+            "sessionId": {
+              "type": "string",
+              "maxLength": 128
+            },
+            "deviceId": {
+              "type": "string",
+              "maxLength": 128
+            }
+          },
+          "required": ["id", "sessionId"]
+        }
+      },
+      "required": ["eventId", "emitterCode", "action", "user"]
+    }
+  end
+  module_function :schema
 end
--- lambda/ruby/spec/lambda_function_spec.rb
+++ lambda/ruby/spec/lambda_function_spec.rb
@@ -1,11 +1,37 @@
+require "json-schema"
 require 'rspec'
 require 'lambda_function'
 
 RSpec.describe Lambda do
-    describe "#add" do
-        it "adds two numbers together" do
-            handler = 
-            expect(Lambda::Handler.add(1,2)).to eq(3) 
-        end
+  describe "#validate_event" do
+    it "doesn't raise an error when parsing a valid event" do
+      valid_event = {
+        "eventId" => "event_id",
+        "emitterCode" => 50,
+        "action" => "sign_in",
+        "user" => {
+          "id" => "id",
+          "sessionId" => "session_id",
+          "deviceId" => "device_id"
+        }
+      }
+
+      expect {Lambda::Handler.validate_event(valid_event) }.not_to raise_error
     end
+
+    it "does raise an error when parsing an invalid event" do
+      invalid_event = {
+        "eventId" => "event_id",
+        "emitterCode" => 5000,
+        "action" => "sign_in",
+        "user" => {
+          "id" => "id",
+          "sessionId" => "session_id",
+          "deviceId" => "device_id"
+        }
+      }
+
+      expect {Lambda::Handler.validate_event(invalid_event) }.to raise_error(JSON::Schema::ValidationError)
+    end
+  end
 end
