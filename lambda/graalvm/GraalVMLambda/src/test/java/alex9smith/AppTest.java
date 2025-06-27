ba2ea9333ce665924efd3c81df565500e29a6db3
Richard Jones <rich@linuxplicable.org>
2025-06-27 07:40:08 +0100

    graalvm lambda works on localstack

--- lambda/graalvm/GraalVMLambda/src/test/java/alex9smith/AppTest.java
+++ lambda/graalvm/GraalVMLambda/src/test/java/alex9smith/AppTest.java
@@ -5,6 +5,7 @@ import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -20,5 +21,21 @@ class AppTest {
     App function = new App();
     SQSEvent result = function.handleRequest(event, null);
     assertEquals(event, result);
+  }
+
+  @Test
+  void handleRequest_full() {
+    List<SQSMessage> records = new ArrayList<SQSMessage>();
+    SQSEvent sqsEvent = new SQSEvent();
+
+    SQSMessage message = new SQSMessage();
+    String body = "{\"eventId\":\"event-id\",\"emitterCode\":1,\"action\":\"action\",\"user\":{\"id\":\"user-id\",\"sessionId\":\"session-id\",\"deviceId\":\"device-id\"}}";
+
+    message.setBody(body);
+    records.add(message);
+    sqsEvent.setRecords(records);
+    App function = new App();
+    SQSEvent result = function.handleRequest(sqsEvent, null);
+    assertEquals(sqsEvent, result);
   }
 }
