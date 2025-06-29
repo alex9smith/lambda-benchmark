7f9e3825d06bfc01cd3e36c84bf304dd9b7ca760
Richard Jones <rich@linuxplicable.org>
2025-06-29 22:10:30 +0100

    infra for all; benchmark for graalvm

--- benchmark/scripts/run_load_test.py
+++ benchmark/scripts/run_load_test.py
@@ -5,7 +5,7 @@ import json
 import boto3
 
 MAX_BATCH_SIZE = 10  # 10 is the limit on the SQS SendMessageBatch API
-EVENTS_PER_LAMBDA = 3000
+EVENTS_PER_LAMBDA = 1
 
 
 class LiveLambdas(Enum):
@@ -14,6 +14,7 @@ class LiveLambdas(Enum):
     ruby = auto()
     python = auto()
     java = auto()
+    graalvm = auto()
 
 
 class User(TypedDict):
--- infra/environment/lambda.tf
+++ infra/environment/lambda.tf
@@ -1,53 +1,53 @@
-# module "typescript_lambda" {
-#   source = "./modules/lambda"
+module "typescript_lambda" {
+  source = "../modules/lambda"
 
-#   source_file         = "../lambda/typescript/dist/index.zip"
-#   language_name       = "typescript"
-#   runtime             = "nodejs20.x"
-#   handler             = "index.handler"
-#   dynamodb_table_name = var.dynamodb_table_name
-# }
+  source_file         = "../../../lambda/typescript/dist/index.zip"
+  language_name       = "typescript"
+  runtime             = "nodejs20.x"
+  handler             = "index.handler"
+  dynamodb_table_name = var.dynamodb_table_name
+}
 
-# module "rust_lambda" {
-#   source = "./modules/lambda"
+module "rust_lambda" {
+  source = "../modules/lambda"
 
-#   source_file         = "../lambda/rust/target/lambda/rust/bootstrap.zip"
-#   language_name       = "rust"
-#   runtime             = "provided.al2023"
-#   handler             = "bootstrap"
-#   dynamodb_table_name = var.dynamodb_table_name
-# }
+  source_file         = "../../../lambda/rust/target/lambda/rust/bootstrap.zip"
+  language_name       = "rust"
+  runtime             = "provided.al2023"
+  handler             = "bootstrap"
+  dynamodb_table_name = var.dynamodb_table_name
+}
 
-# module "ruby_lambda" {
-#   source = "./modules/lambda"
+module "ruby_lambda" {
+  source = "../modules/lambda"
 
-#   source_file         = "../lambda/ruby/deploy.zip"
-#   language_name       = "ruby"
-#   runtime             = "ruby3.3"
-#   handler             = "lambda_function.Lambda::Handler.process"
-#   dynamodb_table_name = var.dynamodb_table_name
-# }
+  source_file         = "../../../lambda/ruby/deploy.zip"
+  language_name       = "ruby"
+  runtime             = "ruby3.3"
+  handler             = "lambda_function.Lambda::Handler.process"
+  dynamodb_table_name = var.dynamodb_table_name
+}
 
-# module "python_lambda" {
-#   source = "./modules/lambda"
+module "python_lambda" {
+  source = "../modules/lambda"
 
-#   source_file         = "../lambda/python/deploy.zip"
-#   language_name       = "python"
-#   runtime             = "python3.12"
-#   handler             = "lambda_function.handler"
-#   dynamodb_table_name = var.dynamodb_table_name
-# }
+  source_file         = "../../../lambda/python/deploy.zip"
+  language_name       = "python"
+  runtime             = "python3.12"
+  handler             = "lambda_function.handler"
+  dynamodb_table_name = var.dynamodb_table_name
+}
 
-# module "java_lambda" {
-#   source = "./modules/lambda"
+module "java_lambda" {
+  source = "../modules/lambda"
 
-#   source_file         = "../lambda/java/JavaLambda/target/JavaLambda.jar"
-#   language_name       = "java"
-#   runtime             = "java21"
-#   handler             = "alex9smith.App::handleRequest"
-#   dynamodb_table_name = var.dynamodb_table_name
-#   timeout             = 10
-# }
+  source_file         = "../../../lambda/java/JavaLambda/target/JavaLambda.jar"
+  language_name       = "java"
+  runtime             = "java21"
+  handler             = "alex9smith.App::handleRequest"
+  dynamodb_table_name = var.dynamodb_table_name
+  timeout             = 10
+}
 
 module "graalvm_lambda" {
   source = "../modules/lambda"
